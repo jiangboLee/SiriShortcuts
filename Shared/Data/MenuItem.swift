cf9466cde4186b53f6ec84b4c74882d7a092d87a
LeeJiangbo <584556825@qq.com>
2018-09-27 19:22:50 +0800

    下班，准备回家继续写

---
+++ Shared/Data/MenuItem.swift
@@ -0,0 +1,28 @@
+//
+//  MenuItem.swift
+//  SiriShortcuts
+//
+//  Created by Lee on 2018/9/27.
+//  Copyright © 2018 Lee. All rights reserved.
+//
+
+import Foundation
+
+public struct MenuItem: Codable, Hashable {
+    
+    public let itemName: String
+    public let shortcutNameKey: String
+    public let price: Decimal
+    public let iconImageName: String
+    public var isAvailable: Bool
+    ///今天特殊菜肴
+    public let isDailySpecial: Bool
+    public init(itemName: String, shortcutNameKey: String, price: Decimal, iconImageName: String, isAvailable: Bool, isDailySpecial: Bool) {
+        self.itemName = itemName
+        self.shortcutNameKey = shortcutNameKey
+        self.price = price
+        self.iconImageName = iconImageName
+        self.isAvailable = isAvailable
+        self.isDailySpecial = isDailySpecial
+    }
+}
